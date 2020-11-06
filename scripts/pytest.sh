3c52cb6aebf620885fe7fc28874fa3725ad84f0e
nadiashaikh <71331161+nadiashaikh@users.noreply.github.com>
2020-11-06 14:34:04 +0000

    Create pytest.sh

---
+++ scripts/pytest.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo apt install python3-pip
+sudo apt-get install python3-venv
+python3 -m venv venv
+. ./venv/bin/activate
+pip3 install -r requirements.txt
+
+pytest ./service-1 --cov ./service-1/application
+pytest ./service-2 --cov ./service-2/application
+pytest ./service-3 --cov ./service-3/application
+pytest ./service-4 --cov ./service-4/application
+
+deactivate
+rm -rf venv
